87e41d98ae8bb8afc18bcca08fa72c3282df8f24
paulh <heinrich@vsp.tu-berlin.de>
2025-05-22 16:31:37 +0200

    small changes do adapt to the new rust qsim version

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ RUST_BASE := ~/git/parallel_qsim_rust
 RUST_EXE := $(RUST_BASE)/target/release
 
 MEMORY ?= 20G
-PCT := 10
+PCT := 1
 
 java := java -Xmx$(MEMORY) -XX:+UseParallelGC -cp $(JAR) org.matsim.prepare.RunParallelQSimBerlinPreparation
 
@@ -33,8 +33,7 @@ $(op)/berlin-$(BV)-vehicleTypes.xml:
 
 $(op)/berlin-$(BV)-vehicleTypes-including-walk.xml: $(op)/berlin-$(BV)-vehicleTypes.xml
 	$(java) prepare adapt-vehicle-types\
-		--input $<\
-		--teleported-modes bike,walk,ride
+		--input $<
 
 $(op)/berlin-$(BV).network.xml.gz:
 	curl https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-$(BV)/input/berlin-$(BV)-network-with-pt.xml.gz -o $@
@@ -66,3 +65,6 @@ convert-network:
 		--ids $(op)/binpb/berlin-$(BV)-$(PCT)pct.ids.binpb\
 		--network $(op)/berlin-$(BV)-$(PCT)pct.network.$(N).binpb\
 		--vehicles $(op)/binpb/berlin-$(BV)-$(PCT)pct.vehicles.binpb
+
+clean:
+	rm -rf $(op)
--- src/main/java/org/matsim/prepare/AdaptVehicleTypes.java
+++ src/main/java/org/matsim/prepare/AdaptVehicleTypes.java
@@ -16,9 +16,6 @@ public class AdaptVehicleTypes implements MATSimAppCommand {
     @CommandLine.Option(names = "--input", description = "Path to the input vehicle types file", required = true)
     private Path input;
 
-    @CommandLine.Option(names = "--teleported-modes", description = "Teleported modes.", split = ",", required = true)
-    private Set<String> teleportedModes;
-
     @Override
     public Integer call() throws Exception {
         Vehicles vehiclesContainer = VehicleUtils.createVehiclesContainer();
@@ -29,13 +26,8 @@ public class AdaptVehicleTypes implements MATSimAppCommand {
         vehType.setMaximumVelocity(1.23)
                 .setPcuEquivalents(0.1)
                 .setNetworkMode("walk")
-                .setFlowEfficiencyFactor(10.0)
-                .getAttributes().putAttribute("lod", "teleported");
+                .setFlowEfficiencyFactor(10.0);
         vehiclesContainer.addVehicleType(vehType);
-
-        teleportedModes.forEach(mode -> {
-            vehiclesContainer.getVehicleTypes().get(Id.create(mode, VehicleType.class)).getAttributes().putAttribute("lod", "teleported");
-        });
 
         MatsimVehicleWriter vehicleWriter = new MatsimVehicleWriter(vehiclesContainer);
         String output = input.toString().replace(".xml", "-including-walk.xml");
